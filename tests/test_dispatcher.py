e23efcfdcbbce8d25148f8eee2fbc9c3a988a2ce
charitybell <hello@charitybell.xyz>
2023-07-26 00:10:57 +0000

    More tests

--- tests/test_dispatcher.py
+++ tests/test_dispatcher.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pytest
 
 from embedded_sass_python import embedded_sass_pb2 as sass_pb
@@ -31,3 +33,40 @@ async def test_compile() -> None:
     response = await dispatcher.dispatch(m)
     expected = '.selector{margin:1;}.selector.nested{margin:2;}'
     assert ''.join(response.compile_response.success.css.split()) == expected
+
+@pytest.mark.asyncio
+async def test_multiple_requests() -> None:
+    dispatcher = await Dispatcher.create()
+
+    m1 = sass_pb.InboundMessage()
+    m2 = sass_pb.InboundMessage()
+
+    m1.compile_request.string.source = '.m1 { .nested { margin: 1; } }'
+    m2.compile_request.string.source = '.m2 { .nested { margin: 2; } }'
+
+    m1_expected = '.m1.nested{margin:1;}'
+    m2_expected = '.m2.nested{margin:2;}'
+
+    m1_response, m2_response = await asyncio.gather(dispatcher.dispatch(m1), dispatcher.dispatch(m2))
+
+    assert ''.join(m1_response.compile_response.success.css.split()) == m1_expected
+    assert ''.join(m2_response.compile_response.success.css.split()) == m2_expected
+
+@pytest.mark.asyncio
+async def test_multiple_sequential_requests() -> None:
+    dispatcher = await Dispatcher.create()
+
+    m1 = sass_pb.InboundMessage()
+    m2 = sass_pb.InboundMessage()
+
+    m1.compile_request.string.source = '.m1 { .nested { margin: 1; } }'
+    m2.compile_request.string.source = '.m2 { .nested { margin: 2; } }'
+
+    m1_expected = '.m1.nested{margin:1;}'
+    m2_expected = '.m2.nested{margin:2;}'
+
+    m1_response = await dispatcher.dispatch(m1)
+    m2_response = await dispatcher.dispatch(m2)
+
+    assert ''.join(m1_response.compile_response.success.css.split()) == m1_expected
+    assert ''.join(m2_response.compile_response.success.css.split()) == m2_expected
